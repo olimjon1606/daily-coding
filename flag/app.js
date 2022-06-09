00776eeb6ed488e46106a66ecb696dc1b9b91447
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2022-06-09 22:54:53 +0300

    Delete flag directory

--- flag/app.js
+++
@@ -1,4 +0,0 @@
-let random = Math.floor(Math.random()*3)+1;
-console.log(random);
-let content = document.getElementById("content");
-content.style.backgroundImage = "url(\"./images/bg" + random +".jpg\")";
